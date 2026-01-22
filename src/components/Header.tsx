9df8d126b5d53960f90ae83bc6d1dda444067904
Liles21 <your.email@example.com>
2026-01-22 11:29:56 +0800

    Update package.json lint script for ESLint v9

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion'
-import { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import './Header.css'
 import ThemeToggle from './ThemeToggle'
